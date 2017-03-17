bfafbc6f970edcf1e0e43dcfe866c9ec94ba116a
courtney <coa.mcallister@gmail.com>
2017-03-16 20:49:14 -0400

    recreated the controller to output a list of todos returned from the api. accomplished using ng-repeat in the html in conjunction with $scope.todos

--- public/js/controllers/todo.controller.js
+++ public/js/controllers/todo.controller.js
@@ -5,7 +5,15 @@
     TodoController.$inject = ['$scope', 'TodoService'];
 
     function TodoController($scope, TodoService){
+      $scope.todos = [];
+      $scope.getTodos = getTodos;
 
-      
+      function getTodos(){
+        console.log('getting the todos yo....');
+        TodoService.getAll()
+                  .then(function(res){
+                    $scope.todos = res.data.todos;
+                  });
+      }
     }
 })()
