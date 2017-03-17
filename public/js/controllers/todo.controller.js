a48e5fb3e052dbceee4f4c4dce0574c8d2f90e19
courtney <coa.mcallister@gmail.com>
2017-03-17 19:58:58 -0400

    implemented the ability for a user to delete a to do. added the delete method to the TodoService.

--- public/js/controllers/todo.controller.js
+++ public/js/controllers/todo.controller.js
@@ -9,16 +9,22 @@
       $scope.newTodo = {};
       $scope.getTodos = getTodos;
       $scope.addTodo = addTodo;
+      $scope.deleteTodo = deleteTodo;
 
       function addTodo(newTodo){
-        console.log('creating a new todo....');
         TodoService.create(newTodo)
                    .then(function(res){
                      getTodos();
                    });
       }
+      function deleteTodo(todo){
+        TodoService.delete(todo)
+                   .then(function(res){
+                     getTodos();
+                   });
+      }
+
       function getTodos(){
-        console.log('getting the todos....');
         TodoService.getAll()
                   .then(function(res){
                     $scope.todos = res.data.todos;
